405027cd0d308832ff851c9c1cbd75171a339502
Nicholas St. Germain <nick@cajun.pro>
2020-08-24 16:21:44 -0500

    additional logging for checks

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"errors"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/itscontained/vault-context/internal/storage"
 )
 
@@ -32,7 +34,7 @@ type VaultEnv struct {
 var Config = Cfg{
 	Files: Files{},
 	Keychain: Keychain{
-		BackendType: "",
+		BackendType: "automatic",
 		Keychain:    keychainBackendConfig{},
 		KDEWallet:   kdeWalletBackendConfig{},
 		SecretService: secretServiceBackendConfig{
@@ -49,13 +51,20 @@ var Config = Cfg{
 	VaultEnvs: make([]VaultEnv, 0),
 }
 
-func (c *Cfg) Exists(url string) bool {
+func (c *Cfg) Exists(v VaultEnv) error {
 	for _, env := range c.VaultEnvs {
-		if env.URL == url {
-			return true
+		if env.URL == v.URL {
+			log.Debug("found matching vault address")
+			if v.Alias == "" {
+				return errors.New("no alias set. limited to one token for instance")
+			}
+			if env.Alias == v.Alias {
+				return errors.New("a context with this alias and vault address already exists")
+			}
+			return nil
 		}
 	}
-	return false
+	return nil
 }
 
 func (c *Cfg) Add(url, namespace, alias string) error {
@@ -64,10 +73,10 @@ func (c *Cfg) Add(url, namespace, alias string) error {
 		Namespace: namespace,
 		Alias:     alias,
 	}
-	if !c.Exists(url) {
+	if err := c.Exists(v); err == nil {
 		c.VaultEnvs = append(c.VaultEnvs, v)
 	} else {
-		return errors.New("context already exists")
+		return err
 	}
 	return nil
 }
