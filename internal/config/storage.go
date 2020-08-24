6492fddd82d335f94abc412e2a95adb963301660
Nicholas St. Germain <nick@cajun.pro>
2020-08-24 15:14:16 -0500

    update config file management

--- internal/config/storage.go
+++ internal/config/storage.go
@@ -56,15 +56,24 @@ func (c *Cfg) FileCheck(enable bool) {
 	if !enable {
 		return
 	}
+	if _, err := os.Stat(c.Files.VaultPath); !os.IsNotExist(err) {
+		log.Debug("deleting existing .vault file")
+		if err := os.Remove(c.Files.VaultPath); err != nil {
+			log.Error("could not delete existing .vault file. Please remove manually and then try again.")
+		}
+	}
 	if file, err := os.OpenFile(c.Files.VaultPath, os.O_WRONLY|os.O_CREATE, 0600); err == nil {
 		var ex string
 		if self, err := os.Executable(); err == nil {
 			ex = self
 		} else {
 			log.Error(err)
+			return
 		}
 		if _, err := file.WriteString(fmt.Sprintf("token_helper = \"%s\"", ex)); err != nil {
 			log.Error(err)
+		} else {
+			log.Debugf("created token-helper file at %s to use %s", c.Files.VaultPath, ex)
 		}
 	} else {
 		log.Fatal("could not create vault config file")
